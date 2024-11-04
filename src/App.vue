167beea9f6cdd2d6ef8d517e2a40097395b83a4f
Artyom <semperidem1989@gmail.com>
2024-11-04 18:00:22 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  data() {
+    return {
+      info: 'Title!',
+      someInfo: 'Anons of message'
+    }
+  }, 
+  methods: {
+    userData(param) {
+      //this.someInfo = 'Some new'
+      this.info = param
+    }
+  }
+}
+</script>
+
+<template>
+  <h3>{{ info }}</h3>
+  <p>{{ someInfo }}</p>
+
+  <button type="button" @click="userData('Some new')">Отправить</button>
+</template>
+
+<style scoped>
+
+h3 {
+  font-weight: lighter;
+}
+
+p {
+  color: red;
+}
+
+</style>
