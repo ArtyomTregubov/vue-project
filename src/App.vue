0525d97a58f751af143fa8a6ef88fa443499c259
Artyom <semperidem1989@gmail.com>
2024-11-06 16:39:26 +0500

    add css

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ export default {
   data() {
     return {
       users: [],
+      error: '',
       userName: '',
       userPass: '',
       userEmail: ''
@@ -10,6 +11,19 @@ export default {
   }, 
   methods: {
     sendData() {
+      if (this.userName == '') {
+        this.error = 'Имя не введено';
+        return;
+      } else if (this.userEmail == '') {
+        this.error = 'Адрес не введён'
+        return
+      } else if (this.userPass == '') {
+        this.error = 'Пароль не введён'
+        return
+      }
+
+      this.error = '';
+
       this.users.push({
         name: this.userName,
         pass: this.userPass,
@@ -22,13 +36,50 @@ export default {
 </script>
 
 <template>
-  <input type="text" v-model="userName" placeholder="Имя"/>
-  <input type="password" v-model="userPass" placeholder="Пароль"/>
-  <input type="email" v-model="userEmail" placeholder="Email"/>
-  <button @click="sendData()">Отправить</button>
-  <p>{{ users }}</p>
+  <main className="main">
+    <form>
+      <input className="form__input" type="text" v-model="userName" placeholder="Имя"/>
+      <input className="form__input" type="password" v-model="userPass" placeholder="Пароль"/>
+      <input className="form__input" type="email" v-model="userEmail" placeholder="Email"/>
+      <button type="button" @click="sendData()">Отправить</button>
+      <p className="error">{{ error }}</p>
+    </form>
+    
+    <div v-for="(el, index) in users" :key="index">
+      <h3>{{ el.name }}</h3>
+      <P>{{ el.email }} - <b>{{ el.pass }}</b></P>
+    </div>
+  </main>
 </template>
 
 <style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 600px;
+}
 
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.form__input {
+  width: 300px;
+  height: 50px;
+  margin: 0 0 10px;
+  padding: 0 0 0 10px;
+}
+
+button {
+  border: 1px solid #1da654;
+  height: 54px;
+  background-color: #5bf080;
+}
+
+.error {
+  color: red;
+}
 </style>
